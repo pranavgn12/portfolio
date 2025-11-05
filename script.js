4c00ea6dfddbbd73270f76a2ba92254f66fad5a5
pranav <pranavgn2005@gmail.com>
2025-11-05 21:35:35 +0530

    fixed overlap

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let cur= document.querySelector("#cur");
 let i=0;
 
+let x;
+let y;
+let px;
+let py;
 
-
+document.addEventListener('mousemove',(e)=>{
+      x =window.innerWidth/parseInt(1/0.15) * parseInt(e.pageX/ (window.innerWidth/parseInt(1/0.15)));
+      y= window.innerHeight/parseInt(1/0.10) * parseInt(e.pageY/ (window.innerHeight/parseInt(1/0.10)));
+})
 
 
 
@@ -22,15 +29,23 @@ function hidecur() {
 showcur();
 
 document.addEventListener('mousemove', (e) => {
+      if(x!=px || y!=py){
       const rect = document.createElement('div');
       rect.classList.add('trail');
-      rect.style.left = `${window.innerWidth/parseInt(1/0.15) * parseInt(e.pageX/ (window.innerWidth/parseInt(1/0.15)))}px`;
-      rect.style.top = `${window.innerHeight/parseInt(1/0.10) * parseInt(e.pageY/ (window.innerHeight/parseInt(1/0.10)))}px`;
+      rect.style.left = `${x}px`;
+      rect.style.top = `${y}px`;
+      px=x;
+      py=y;
       document.body.appendChild(rect);
+      //while(x==px && y==py);
+       requestAnimationFrame(() => rect.style.opacity = '0');
+      setTimeout(() => rect.remove(), 2000);
+      }
       // trigger fade out
-      requestAnimationFrame(() => rect.style.opacity = '0');
+      /*while((window.innerWidth/parseInt(1/0.15) * parseInt(ei.pageX/ (window.innerWidth/parseInt(1/0.15))))==x  || window.innerHeight/parseInt(1/0.10) * parseInt(ei.pageY/ (window.innerHeight/parseInt(1/0.10)))== y ){
 
+      }*/
+     
       // remove element after fade
-      
-      setTimeout(() => rect.remove(), 500);
+     
 });
