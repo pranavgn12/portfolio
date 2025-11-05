4a26037bbf8e16d536a4ac84cd43f70c21a0bb79
pranav <pranavgn2005@gmail.com>
2025-11-05 14:27:15 +0530

    2

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let cur= document.querySelector("#cur");
 let i=0;
 
+
+
+
+
+
 function showcur() {
   cur.innerText="_";
     i++;
@@ -15,3 +20,17 @@ function hidecur() {
 
 
 showcur();
+
+document.addEventListener('mousemove', (e) => {
+      const rect = document.createElement('div');
+      rect.classList.add('trail');
+      rect.style.left = `${window.innerWidth/parseInt(1/0.15) * parseInt(e.pageX/ (window.innerWidth/parseInt(1/0.15)))}px`;
+      rect.style.top = `${window.innerHeight/parseInt(1/0.10) * parseInt(e.pageY/ (window.innerHeight/parseInt(1/0.10)))}px`;
+      document.body.appendChild(rect);
+      // trigger fade out
+      requestAnimationFrame(() => rect.style.opacity = '0');
+
+      // remove element after fade
+      
+      setTimeout(() => rect.remove(), 500);
+});
