6e35b882c3c26012ffa3fa95d0576496363dfaa4
pranav <pranavgn2005@gmail.com>
2025-11-05 21:53:11 +0530

    rainbow

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ let y;
 let px;
 let py;
 
-document.addEventListener('mousemove',(e)=>{
-      x =window.innerWidth/parseInt(1/0.15) * parseInt(e.pageX/ (window.innerWidth/parseInt(1/0.15)));
-      y= window.innerHeight/parseInt(1/0.10) * parseInt(e.pageY/ (window.innerHeight/parseInt(1/0.10)));
-})
 
 
 
@@ -28,12 +24,21 @@ function hidecur() {
 
 showcur();
 
+
+
+document.addEventListener('mousemove',(e)=>{
+      x =window.innerWidth/parseInt(1/0.15) * parseInt(e.pageX/ (window.innerWidth/parseInt(1/0.15)));
+      y= window.innerHeight/parseInt(1/0.20) * parseInt(e.pageY/ (window.innerHeight/parseInt(1/0.20)));
+})
+
+
 document.addEventListener('mousemove', (e) => {
       if(x!=px || y!=py){
       const rect = document.createElement('div');
       rect.classList.add('trail');
       rect.style.left = `${x}px`;
       rect.style.top = `${y}px`;
+      rect.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 90%)`;
       px=x;
       py=y;
       document.body.appendChild(rect);
